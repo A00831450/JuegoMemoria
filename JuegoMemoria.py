31c28b775575a40aac3a58d0c43ac6d2c00e1979
A00831450 <A00831450@itesm.mx>
2021-09-17 10:19:47 -0500

    Update JuegoMemoria.py

--- JuegoMemoria.py
+++ JuegoMemoria.py
@@ -7,6 +7,7 @@ tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 countTap = 0
+uncovered = 0
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -32,16 +33,19 @@ def tap(x, y):
     "Update mark and hidden tiles based on tap."
     spot = index(x, y)
     mark = state['mark']
-    countTap = 0
-    print("TAPS:",countTap)
+    global countTap
+    global uncovered
+    countTap = countTap + 1
+    print("TAPS:", countTap)
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
-        countTap = countTap +1
     else:
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        uncovered = uncovered+1
+        print("Uncovered:", uncovered)
 
 def draw():
     "Draw image and tiles."
